a497abaee28b187a31525f353c712440d0bdb7c2
CABS72030202 <sebastien.cabana@uqtr.ca>
2024-03-28 22:48:35 -0400

    Created generated folder as default repository for text files.

--- src/file.c
+++ src/file.c
@@ -3,6 +3,26 @@
 #include "../src/file.h"
 #include "../src/user.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+char* curr_repo = "./generated/";
+char* curr_file_name = "boolean_equation_";
+int curr_file_count = 0;
+
+void count_files() {
+    DIR *dir;
+    struct dirent *entry;
+    if ((dir = opendir(curr_repo)) != NULL) {
+        while ((entry = readdir(dir)) != NULL) 
+            if (entry->d_type == DT_REG)
+                curr_file_count++;
+        closedir(dir);
+    } else {
+        perror("opendir");
+        exitError("count_files | file.c");
+    }
+}
 
 void save_equation(Equation* eq) {
     
--- src/file.h
+++ src/file.h
@@ -1,9 +1,15 @@
 #ifndef FILE_H
 #define FILE_H
 
-/*  Functions used to manipulate text files     
+/*  Functions and variables used for
+    text file management     
 */
 
-void save_equation(Equation*);                   // Function used to save a boolean equation in a text file
+extern char* curr_repo;                         // Variable used to define the default repository where the file will be saved
+extern char* curr_file_name;                    // Variable used to define the default name of a text file
+extern int curr_file_count;                     // Variable used to keep track of the number of files produced during execution
+
+void count_files();                             // Function counting the number of files in the current repository
+void save_equation(Equation*);                  // Function used to save a boolean equation in a text file
 
 #endif
